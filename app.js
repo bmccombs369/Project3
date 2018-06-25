6daa6093667f157ca32dfe7b6567173f2208df7b
bmccombs369 <bmccombs369@gmail.com>
2018-06-25 14:37:42 -0400

    created express/react boilerplate

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
+
+const mongoose = require ('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI);
+
+const connection = mongoose.connection;
+connection.on('connected', () => {
+  console.log('Mongoose Connected Successfully');
+});
+connection.on('error', (err) => {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(__dirname + '/client/build/'));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+
+module.exports = app;

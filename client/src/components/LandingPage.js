04a21b1bcb8c985a134122d4cdfb6d52ff3a329e
bmccombs369 <bmccombs369@gmail.com>
2018-06-27 20:39:42 -0400

    created LandingPage component

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 12vw;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+`
+
+const Body = styled.div`
+  background-image: url('http://yesofcorsa.com/wp-content/uploads/2017/01/Vape-Wallpaper-Free-Download.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+`
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <Body>
+        <Title>
+          <StyledLink to='/users'>Home Page</StyledLink>
+        </Title>
+      </Body>
+    );
+  }
+}
+
+export default LandingPage;

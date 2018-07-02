3a4943fa09cc79cfd311b44a469af40ad0952b23
bmccombs369 <bmccombs369@gmail.com>
2018-07-02 10:32:53 -0400

    styled LandingPage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const Title = styled.h1`
+const Title = styled.div`
   text-align: center;
   font-size: 12vw;
 `
 
 const StyledLink = styled(Link)`
   text-decoration: none;
-  /* color: white; */
+  color: white;
 `
 
 const Body = styled.div`
-  /* background-image: url('http://yesofcorsa.com/wp-content/uploads/2017/01/Vape-Wallpaper-Free-Download.jpg'); */
+  background-image: url('http://yesofcorsa.com/wp-content/uploads/2017/01/Vape-Wallpaper-Free-Download.jpg');
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;

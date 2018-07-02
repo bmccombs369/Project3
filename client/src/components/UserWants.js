8e7f909c08c90c3aba76344777a3ea03e6b23aa5
bmccombs369 <bmccombs369@gmail.com>
2018-07-02 10:50:50 -0400

    made UserWants match UserHaves

--- client/src/components/UserWants.js
+++ client/src/components/UserWants.js
@@ -10,19 +10,22 @@ const Card = styled.div`
 const Container = styled.div`
   padding: 5px 16px;
   text-align: center;
+  background-color: #ebecdb;
+  border-radius: 0 0 5px 5px;
 `
 
 const CardContainer = styled.div`
   padding: 5px 16px;
   height: 33%;
   width: 40%;
-  color: #BFC289;
+  color: #5f6144;
   font-size: 2vw;
 `
 
 const StyledImg = styled.img`
   width: 100%;
   border-radius: 5px 5px 0 0;
+
 `
 
 const Page = styled.div`
